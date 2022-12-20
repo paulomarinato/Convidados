585638c597ff561c7b1e1b60c2d9f8dc51f70f63
paulomarinato <marinatopaulo@gmail.com>
2022-12-20 14:49:18 -0300

    Alteração dos Fragments e view models

--- app/src/main/java/com/criandoapps/convidados/ui/AbsentViewModel.kt
+++ app/src/main/java/com/criandoapps/convidados/ui/AbsentViewModel.kt
@@ -1,13 +1,13 @@
-package com.criandoapps.convidados.ui.home
+package com.criandoapps.convidados.ui
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class AbsentViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is slideshow Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/criandoapps/convidados/ui/AllGuestsViewModel.kt
+++ app/src/main/java/com/criandoapps/convidados/ui/AllGuestsViewModel.kt
@@ -1,13 +1,13 @@
-package com.criandoapps.convidados.ui.gallery
+package com.criandoapps.convidados.ui
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class AllGuestsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
 }
