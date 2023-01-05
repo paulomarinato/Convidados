302644e284b9f8b15758a3b525b18c29d327ef03
paulomarinato <marinatopaulo@gmail.com>
2023-01-05 14:56:16 -0300

    Criação do Repositório e Singleton

---
+++ app/src/main/java/com/criandoapps/convidados/GuesFormViewModel.kt
@@ -0,0 +1,6 @@
+package com.criandoapps.convidados
+
+import androidx.lifecycle.ViewModel
+
+class GuesFormViewModel : ViewModel() {
+}
--- app/src/main/java/com/criandoapps/convidados/GuestFormActivity.kt
+++ app/src/main/java/com/criandoapps/convidados/GuestFormActivity.kt
@@ -2,10 +2,28 @@ package com.criandoapps.convidados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.criandoapps.convidados.databinding.ActivityGuestFormBinding
 
-class GuestFormActivity : AppCompatActivity() {
+class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityGuestFormBinding
+    private lateinit var viewModel: GuesFormViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_form)
+
+        binding = ActivityGuestFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        viewModel = ViewModelProvider(this).get(GuesFormViewModel::class.java)
+        binding.buttonSave.setOnClickListener(this)
+        binding.radioPresent.isChecked = true
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save){
+
+        }
     }
 }
