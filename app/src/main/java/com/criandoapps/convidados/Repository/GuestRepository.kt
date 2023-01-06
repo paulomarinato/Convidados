dd9888cad9eb710924c3631c694c48b13bdeffc8
paulomarinato <marinatopaulo@gmail.com>
2023-01-06 19:44:30 -0300

    função remoção do banco de dados

--- app/src/main/java/com/criandoapps/convidados/Repository/GuestRepository.kt
+++ app/src/main/java/com/criandoapps/convidados/Repository/GuestRepository.kt
@@ -9,6 +9,7 @@ class GuestRepository private constructor(context: Context){
 
     private val guestDataBase = GuestDataBase(context)
 
+    // Singleton
     companion object{
         private lateinit var repository: GuestRepository
 
@@ -37,6 +38,38 @@ class GuestRepository private constructor(context: Context){
         }
     }
 
-    fun update(){
+    fun update(guest: GuestModel): Boolean{
+        return try {
+            val db = guestDataBase.writableDatabase
+            val presence = if (guest.presence) 1 else 0
+
+            val values = ContentValues()
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+
+            val selections = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(guest.id.toString())
+
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, values, selections, args)
+            true
+        }catch (e: java.lang.Exception){
+            false
+        }
+    }
+
+    fun delete(id: GuestModel): Boolean{
+        return try {
+            val db = guestDataBase.writableDatabase
+
+            val selections = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(id.toString())
+
+
+            db.delete(DataBaseConstants.GUEST.TABLE_NAME, selections, args)
+            true
+        }catch (e: java.lang.Exception){
+            false
+        }
     }
 }
