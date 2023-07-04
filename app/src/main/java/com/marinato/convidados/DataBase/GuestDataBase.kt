48a651893ccc4780bc44d6ed62ee829769872508
paulomarinato <marinatopaulo@gmail.com>
2023-07-04 00:16:27 -0300

    criação da clase GuestDataBase

---
+++ app/src/main/java/com/marinato/convidados/DataBase/GuestDataBase.kt
@@ -0,0 +1,19 @@
+package com.marinato.convidados.DataBase
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class GuestDataBase( context: Context) : SQLiteOpenHelper(context, NAME, null, VERSION) {
+
+    companion object{
+        private const val NAME = "guestDB"
+        private const val VERSION = 1
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+    }
+}
